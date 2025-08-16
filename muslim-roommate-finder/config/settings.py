cb50d18eab09a4072cc4528759863258069fd3e4
mastershifu24 <ahmedshifa298@gmail.com>
2025-08-16 10:51:06 -0400

    setting Debug to False

--- muslim-roommate-finder/config/settings.py
+++ muslim-roommate-finder/config/settings.py
@@ -9,7 +9,7 @@ load_dotenv(BASE_DIR / '.env')
 
 # SECURITY
 SECRET_KEY = os.getenv('SECRET_KEY', 'change-this-to-a-secure-key')
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = False
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '.onrender.com').split(',')
 
 # APPLICATION DEFINITION
