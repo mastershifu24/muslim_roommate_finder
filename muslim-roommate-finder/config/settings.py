f69bace149467626ec9a6cc8e1027ccb3b4147ab
mastershifu24 <ahmedshifa298@gmail.com>
2025-09-02 08:27:24 -0400

    testing

--- muslim-roommate-finder/config/settings.py
+++ muslim-roommate-finder/config/settings.py
@@ -9,7 +9,7 @@ load_dotenv(BASE_DIR / '.env')
 
 # SECURITY
 SECRET_KEY = os.getenv('SECRET_KEY', 'change-this-to-a-secure-key')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["muslim-roommate-finder.onrender.com"]
 
 
